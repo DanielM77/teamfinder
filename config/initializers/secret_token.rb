ee7a2f600fd2fd5021361459803e7bad2dccede4
Daniel Muenstermann <daniel.muenstermann@online.de>
2012-05-11 17:35:38 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamfinder::Application.config.secret_token = 'd7ff61aeac8cf736fdfe37a49980751109efc22b20e0c6c3babcb9eee6ec9b7bfc9f8ad7377cb4de39be804a97ddbe70bb9756ad7833e18c1663b45682c01464'
