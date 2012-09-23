c29c5dc66b6041782c92490980ede00e31c9fdf1
daniel <daniel.muenstermann@online.de>
2012-09-23 20:16:18 +0200

    disabled asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -57,10 +57,10 @@ module Teamfinder
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
     config.app_generators.stylesheet_engine :sass
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w[active_admin.css active_admin.js print.css]
