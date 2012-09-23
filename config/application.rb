2bb13e5e284b7c50df516770ea59bb6c3a834d1c
daniel <daniel.muenstermann@online.de>
2012-09-23 20:02:45 +0200

    Changed asset version

--- config/application.rb
+++ config/application.rb
@@ -58,9 +58,9 @@ module Teamfinder
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.app_generators.stylesheet_engine :less
+    config.app_generators.stylesheet_engine :sass
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w[active_admin.css active_admin.js print.css]
