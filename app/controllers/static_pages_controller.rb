2478d924d694e745414ee1017fd9bacbe039826c
daniel <daniel.muenstermann@online.de>
2012-11-15 23:25:02 +0100

    Bugfix show_profile

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
   end
   def index
     @top_five_club_offers = PublicOffer.valid_only.limit(5).order('created_at DESC')
-    @top_five_player_offers = PlayerProfile.where(show_profile = 1).limit(5).order('created_at DESC')
+    @top_five_player_offers = PlayerProfile.where("show_profile = ?", true).limit(5).order('created_at DESC')
     #redirect_to market_index_path if user_signed_in?
   end
   def pricing
