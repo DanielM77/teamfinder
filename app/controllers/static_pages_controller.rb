30e732d091e2f39790f3ac032bb5202c9ac0951b
daniel <daniel.muenstermann@online.de>
2012-11-15 23:03:34 +0100

    Front Design with new widgets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,8 @@ class StaticPagesController < ApplicationController
   def about
   end
   def index
+    @top_five_club_offers = PublicOffer.valid_only.limit(5).order('created_at DESC')
+    @top_five_player_offers = PlayerProfile.where(show_profile = 1).limit(5).order('created_at DESC')
     #redirect_to market_index_path if user_signed_in?
   end
   def pricing
